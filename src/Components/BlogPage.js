cef8d7d4b7f39c5044aa08ed4474dcfe9f26c346
Akari Yoshiya <akariyoshiya@gmail.com>
2023-07-04 21:33:12 -0700

    first commit

---
+++ src/Components/BlogPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div className="container">
+      <h1>ブログ</h1>
+
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+      <p>text text text text text</p>
+    </div>
+  );
+};
+
+export default BlogPage;
